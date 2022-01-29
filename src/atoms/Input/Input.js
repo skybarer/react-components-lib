84a357ec39e1dc65ad3b840d4cb5b0e1b86ba86c
skybarer <akashdhar.apssdc@gmail.com>
2022-01-29 15:16:18 +0530

    css file added in all components

---
+++ src/atoms/Input/Input.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+// https://github.com/iiison/react-form
+
+const Input = (
+    {
+        id = 'input-1',
+        type = 'text',
+        defaultValue = 'akash',
+        placeholder = 'Your username or email',
+        className,
+        style,
+        onChange
+    }) => {
+
+    onChange = (e) => {
+        this.defaultValue = e.target.value
+    }
+
+
+    return (
+        <input
+            id={id}
+            type={type}
+            className={className}
+            placeholder={placeholder}
+            onChange={({ target: { value } }) => onChange(value)}
+            aria-label="Your username or email"
+            aria-invalid="false"
+            value={defaultValue}
+            name="username"
+            aria-required="true"
+            aria-describedby="tooltip-input-1"
+            style={{
+                color: 'black',
+                backgroundColor: '#f3f7f7',
+                border: '0',
+                // border: '1px solid #c2c7d0',
+                borderRadius: '0',
+                minWidth: '320px',
+                width: '100%',
+                height: '40px',
+                outline: 'none',
+                fontSize: '18px',
+                padding: '1px 10px',
+                boxShadow: 'inset 0 1px 4px rgb(0 0 0 / 10%), 0 0 0 transparent',
+                marginBottom: '0',
+                boxSizing: 'border-box',
+                fontWeight: '400',
+                style
+
+            }}>
+        </input>
+    );
+}
+
+Input.propTypes = {
+    id: PropTypes.string,
+    type: PropTypes.string,
+    defaultValue: PropTypes.string,
+    placeholder: PropTypes.string,
+    className: PropTypes.string,
+    style: PropTypes.object,
+    onChange: PropTypes.func,
+};
+
+Input.defaultProps = {
+    onChange: undefined,
+};
+
+export default Input;
+
---
+++ src/atoms/Input/input.stories.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import Input from './Input';
+import {
+  Title,
+  Subtitle,
+  Description,
+  Primary,
+  ArgsTable,
+  Stories,
+  PRIMARY_STORY,
+} from '@storybook/addon-docs';
+
+export default {
+  title: 'atoms/Input/Basic',
+  component: Input,
+  parameters: {
+    options: { selectedPanel: 'storybook/a11y/panel' },
+    docs: {
+      page: () => (
+        <>
+          <Title />
+          <Subtitle />
+          <Description />
+          <Primary />
+          <ArgsTable story={PRIMARY_STORY} />
+          <Stories />
+        </>
+      ),
+    },
+  },
+  argTypes: {
+    backgroundColor: { control: 'color' },
+    // onClick: { action: true },
+  }
+};
+
+const Template = (args) => <Input {...args} />;
+
+export const Basic = Template.bind({});
+Basic.parameters = {
+  layout: 'centered',
+  // docs: {
+  //   page: null,
+  // },
+};
+
+Basic.args = {
+  label: 'My Button',
+  backgroundColor: 'aliceblue',
+  color: '#000000'
+};
+
